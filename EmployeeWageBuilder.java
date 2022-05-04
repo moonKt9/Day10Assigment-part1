4ba80c21533cdfa61aee935baddfdfec436cdcd9
Kritika <kritistarsmind@gmail.com>
2022-05-04 18:23:28 +0530

    Added:Pat time wage

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -4,7 +4,9 @@ public class EmployeeWageBuilder {
 	
 	public static void main(String[] args) {
 		//constant
-		int IS_FULL_TIME=1;
+		int IS_FULL_TIME=2;
+		int IS_PART_TIME=1;
+		
 		
 		int EMP_RATE_TIME=1;
 		int EMP_RATE_PER_HOUR=20;
@@ -16,10 +18,13 @@ public class EmployeeWageBuilder {
 	//check emp daily wage
 		double empCheck=Math.floor(Math.random()*10)%2;
 		
-		if(empCheck==IS_FULL_TIME)
+		if(empCheck==IS_PART_TIME)
+			empHrs=4;
+		else if (empCheck==IS_FULL_TIME)
 			empHrs=8;
 		else
 			empHrs=0;
+			
 		
 		empWage=empHrs*EMP_RATE_PER_HOUR;
 		

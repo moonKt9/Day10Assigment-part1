ac3b9a7c28d88457f29b71293b8efdeeef81bf9a
Kritika <kritistarsmind@gmail.com>
2022-05-04 18:38:16 +0530

    Added:Using switch cases

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -1,29 +1,32 @@
 package com.bridgelabz2;
 
+
+
 public class EmployeeWageBuilder {
 	
+	public static final int IS_PART_TIME=1;
+	public static final int IS_FULL_TIME=2;
+	public static final int EMP_RATE_PER_HOUR=20;
+	
 	public static void main(String[] args) {
-		//constant
-		int IS_FULL_TIME=2;
-		int IS_PART_TIME=1;
 		
-		
-		int EMP_RATE_TIME=1;
-		int EMP_RATE_PER_HOUR=20;
 		//variables
 		int empHrs=0;
 		int empWage=0;
 		//computation
 		
 	//check emp daily wage
-		double empCheck=Math.floor(Math.random()*10)%2;
-		
-		if(empCheck==IS_PART_TIME)
+		int empCheck= (int)(Math.random()*10)%3;
+		switch(empCheck) {
+		case IS_PART_TIME:
 			empHrs=4;
-		else if (empCheck==IS_FULL_TIME)
+			break;
+		case IS_FULL_TIME:
 			empHrs=8;
-		else
-			empHrs=0;
+			break;
+			default:
+				empHrs=0;
+		}
 			
 		
 		empWage=empHrs*EMP_RATE_PER_HOUR;

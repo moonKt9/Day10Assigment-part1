0879f6ce166a286a63171c4d9815e1530f0889bb
Kritika <kritistarsmind@gmail.com>
2022-05-06 10:53:30 +0530

    Added:Using method

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -7,15 +7,21 @@ public class EmployeeWageBuilder {
 	public static final int IS_PART_TIME=1;
 	public static final int IS_FULL_TIME=2;
 	public static final int EMP_RATE_PER_HOUR=20;
+	public static final int NUM_OF_WORKING_DAYS=20;
+	public static final int MAX_HRS_IN_MONTH=100;
 	
-	public static void main(String[] args) {
+	public static int computeEmpWage() {
 		
 		//variables
 		int empHrs=0;
-		int empWage=0;
+	int	totalEmpHrs=0;
+	int totalWorkingDays=0;
+	int	totalEmpWage=0;
 		//computation
+	while(totalEmpHrs<=MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+		totalWorkingDays++;
+	
 		
-	//check emp daily wage
 		int empCheck= (int)(Math.random()*10)%3;
 		switch(empCheck) {
 		case IS_PART_TIME:
@@ -29,10 +35,23 @@ public class EmployeeWageBuilder {
 		}
 			
 		
-		empWage=empHrs*EMP_RATE_PER_HOUR;
-		
-			System.out.println("Employee wage:"+empWage);
-		
+	
+		 totalEmpHrs+=empHrs;
+		System.out.println("Day#:"+ totalWorkingDays +"Emp Hr:"+empHrs);
 	}
+	
+	 totalEmpWage=totalEmpHrs*EMP_RATE_PER_HOUR;
 
+	 System.out.println("Total Employee wage:"+totalEmpWage);
+	 return totalEmpWage;
+			
+	}
+	
+	public static void main(String[] args) {
+		computeEmpWage();
+	}
 }
+
+	
+
+
